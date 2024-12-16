49e6dfd3dc0c50b6efe4d457f768d0f35b733006
delee <delee.dev@gmail.com>
2024-12-16 23:50:27 +0900

    feat: 포인트 내역 조회 기능 구현 (Green)

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,5 +1,6 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,8 @@ import java.util.List;
 @Service
 public class PointService {
     private final UserPointTable userPointTable;
+    private final PointHistoryTable pointHistoryTable;
+
 
     /**
      * 포인트 조회 함수
@@ -29,6 +32,6 @@ public class PointService {
     }
 
     public List<PointHistory> getHistories(long userId) {
-        return null;
+        return pointHistoryTable.selectAllByUserId(userId);
     }
 }
