d5ff501ae7098c7d2fea8b6c41ac869fd47ac2ba
delee <delee.dev@gmail.com>
2024-12-17 10:49:44 +0900

    comment: 포인트 조회 함수의 행동 분석 주석 추가

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -8,6 +8,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class PointService {
     private final UserPointTable userPointTable;
+
+    /**
+     * 포인트 조회 함수
+     *
+     * 행동 분석
+     * 1. id 값을 파라미터로 넘겨 받는다.
+     * 2. 포인트 조회 API 를 호출한다.
+     * 3. 조회된 결과를 반환한다.
+     *
+     * Test Case
+     * 1. 성공
+     *  - 시스템에 포인트를 충전한 경우, 해당 UserPoint 객체를 반환한다.
+     *  - 시스템에 포인트를 충전하지 않은 경우, 기본 값을 반환한다.
+     **/
     public UserPoint getPoint(long id) {
         return userPointTable.selectById(id);
     }
