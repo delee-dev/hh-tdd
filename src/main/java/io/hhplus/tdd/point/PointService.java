1833352d083a7eb96980a18c4658acbdbfd3c188
delee <delee.dev@gmail.com>
2024-12-17 15:14:16 +0900

    test: 포인트 충전 기능 실패 테스트 작성 (Red)

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -47,4 +47,31 @@ public class PointService {
     public List<PointHistory> getHistories(long userId) {
         return pointHistoryTable.selectAllByUserId(userId);
     }
+
+    /**
+     * 포인트를 충전하는 함수
+     *
+     * 정책
+     * 1. 포인트는 최대 한도(1,000,000)를 초과할 수 없다.
+     * 2. 충전할 금액은 0보다 큰 값이어야 한다.
+     *
+     * 행동 분석
+     * 1. id 와 충전 예정 포인트를 파라미터로 넘겨받는다.
+     * 2. 충전할 포인트가 정책(2)을 준수하는지 검사한다.
+     * 3. 조회 API 로 기존 포인트를 조회한다.
+     * 4. 충전 후의 포인트가 정책(1)을 준수하는지 검사한다.
+     * 5. 충전 API 를 호출한다.
+     * 6. 충전 내역 추가 API 를 호출한다.
+     *
+     * Test Case
+     * 1. 성공
+     *  - 충전 후 금액은 (충전 전 금액 + 충전 금액)과 일치한다.
+     *  - 포인트 충전 내역이 추가된다.
+     * 2. 실패
+     *  - 파라미터로 받은 충전할 금액이 0보다 같거나 작으면 실패한다.
+     *  - 충전 후 잔고가 최대 한도를 초과하면 실패한다.
+     * */
+    public UserPoint charge(long id, long amount) {
+        return UserPoint.empty(id);
+    }
 }
--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -15,7 +15,7 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class PointServiceTest {
@@ -131,4 +131,101 @@ class PointServiceTest {
         }
     }
 
+    @Nested
+    @DisplayName("포인트 충전")
+    class ChargePointTest {
+        /**
+         * Test Case: 충전 후 금액은 (충전 전 금액 + 충전 금액)과 일치해야 합니다.
+         * 작성 이유
+         *  - 포인트 충전 기능이 정확하게 동작하는지 검증합니다.
+         * */
+        @Test
+        void 충전_후_잔고는_충전_전_잔고에_충전_금액을_더한_값과_일치한다() {
+            // given
+            long id = 1L;
+            long existingPoint = 200L;
+            long pointToCharge = 100L;
+            long expectedPoint = existingPoint + pointToCharge;
+
+            UserPoint existingUserPoint = new UserPoint(id, existingPoint, System.currentTimeMillis());
+            UserPoint updatedUserPoint = new UserPoint(id, expectedPoint, System.currentTimeMillis());
+
+            when(userPointTable.selectById(id))
+                    .thenReturn(existingUserPoint);
+            when(userPointTable.insertOrUpdate(id, expectedPoint))
+                    .thenReturn(updatedUserPoint);
+
+            // when
+            UserPoint actual = pointService.charge(id, pointToCharge);
+
+            // then
+            assertThat(actual.point())
+                    .isEqualTo(expectedPoint);
+        }
+        /**
+         * Test Case: 충전 후에는 포인트 충전 내역이 추가되어야 합니다.
+         * 작성 이유:
+         *  - 포인트 충전 시, 충전 내역이 기록되는지 검증합니다.
+         * */
+        @Test
+        void 충전_후에는_포인트_충전_내역이_추가된다() {
+            // given
+            long id = 1L;
+            long existingPoint = 200L;
+            long pointToCharge = 100L;
+            long expectedPoint = existingPoint + pointToCharge;
+
+            UserPoint existingUserPoint = new UserPoint(id, existingPoint, System.currentTimeMillis());
+            UserPoint updatedUserPoint = new UserPoint(id, expectedPoint, System.currentTimeMillis());
+
+            when(userPointTable.selectById(id))
+                    .thenReturn(existingUserPoint);
+            when(userPointTable.insertOrUpdate(id, expectedPoint))
+                    .thenReturn(updatedUserPoint);
+
+            // when
+            pointService.charge(id, pointToCharge);
+
+            // then
+            verify(pointHistoryTable, times(1))
+                    .insert(eq(id), eq(pointToCharge), eq(TransactionType.CHARGE), eq(updatedUserPoint.updateMillis()));
+        }
+
+        /**
+         * Test Case: 충전할 금액이 0과 작거나 같으면 충전에 실패합니다.
+         * 작성 이유
+         *  - "충전할 금액은 0보다 큰 값이어야 한다"는 정책을 만족하는지 확인합니다.
+         * */
+        @Test
+        void 충전할_금액이_0과_작거나_같으면_충전을_실패한다() {
+            // given
+            long id = 1L;
+            long amount = -100L;
+
+            // when & then
+            assertThatThrownBy(() -> pointService.charge(id, amount))
+                    .isInstanceOf(RuntimeException.class);
+        }
+
+        /**
+         * Test Case: 충전 후 잔고가 최대 한도(1,000,000)를 초과하면 충전에 실패합니다.
+         * 작성 이유
+         *  - "포인트는 최대 한도(1,000,000)를 초과할 수 없다"는 정책을 만족하는지 확인합니다.
+         * */
+        @Test
+        void 충전_후_잔고가_최대_한도를_초과하면_충전을_실패한다() {
+            // given
+            long id = 1L;
+            long currPoint = 999_999L;
+            long chargeAmount = 100L;
+
+            UserPoint userPoint = new UserPoint(id, currPoint, System.currentTimeMillis());
+            when(userPointTable.selectById(id))
+                    .thenReturn(userPoint);
+
+            // when & then
+            assertThatThrownBy(() -> pointService.charge(id, chargeAmount))
+                    .isInstanceOf(RuntimeException.class);
+        }
+    }
 }
