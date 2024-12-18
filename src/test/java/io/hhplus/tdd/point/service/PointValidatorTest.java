586631fb12df9ddc93af62c80609657552b3e37e
delee <delee.dev@gmail.com>
2024-12-18 16:18:09 +0900

    test: point 패키지 구조 변경에 맞춰 테스트 코드 이동

--- src/test/java/io/hhplus/tdd/point/service/PointValidatorTest.java
+++ src/test/java/io/hhplus/tdd/point/service/PointValidatorTest.java
@@ -1,6 +1,5 @@
-package io.hhplus.tdd.point;
+package io.hhplus.tdd.point.service;
 
-import io.hhplus.tdd.point.service.PointValidator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
