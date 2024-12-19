614158b96f0807369e6620b2ee1d6c2948a84c26
delee <delee.dev@gmail.com>
2024-12-19 22:52:43 +0900

    comment: 동시성 제어 테스트 주석 추가

--- src/test/java/io/hhplus/tdd/point/integration/ConcurrencyControlTest.java
+++ src/test/java/io/hhplus/tdd/point/integration/ConcurrencyControlTest.java
@@ -22,6 +22,11 @@ public class ConcurrencyControlTest {
     @Autowired
     private UserPointRepository userPointRepository;
 
+    /**
+     * Test Case: 동일한 금액의 충전 요청을 여러번 보낸 후, 포인트 금액이 (충전 금액 * 요청 횟수)가 맞는지 확인합니다.
+     * 작성 이유
+     *  - 동일한 id로 여러 요청을 보내는 경우 한 번에 하나씩 처리되는지 확인합니다.
+     * */
     @Test
     void 동일한_사용자가_동시에_요청을_보내는_경우_한_번에_하나씩_처리된다() throws InterruptedException {
         // given
@@ -51,6 +56,12 @@ public class ConcurrencyControlTest {
         assertThat(userPoint.point()).isEqualTo(pointToCharge * threadCount);
     }
 
+    /**
+     * Test Case: 충전 및 사용 요청을 반복적으로 보내는 경우, 예외가 발생하지 않는지 확인합니다.
+     * 작성 이유
+     *  - 동일한 id로 여러 요청을 보내는 경우 순차적으로 처리되는지 확인합니다.
+     *  - 동일 금액의 충전, 사용을 반복할 때 순서대로 처리되지 않는다면, 사용 시 충전 금액이 모자라 예외가 발생합니다.
+     * */
     @Test
     void 동일한_사용자가_동시에_요청을_보내는_경우_순서대로_처리된다() throws InterruptedException {
         // given
